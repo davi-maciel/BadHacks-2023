fa1ae34bb9038cd84bd410756dbe645e09b33d97
tkslucas <takayaslucas@gmail.com>
2023-02-11 16:35:24 -0600

    formata ai eliseu

--- gf_taka.py
+++ gf_taka.py
@@ -16,27 +16,33 @@
 import os
 import openai
 import json
+import typer
+import rich
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+prompt = "The following is a conversation with your girlfriend. Your girlfriend is cute, smart, a student at Northwestern University, theater major"
+
 while True:
+    print("You: ")
     userInput = input()
     if userInput == "exit":
         break
     try:
-        prompt = "You: What have you been up to?\nGirlfriend: Thinking about you\nYou: That's cute. How are you doing?\nGirlfriend:" + userInput
+        prompt += "\n\nYou: " + userInput + "\n"
         data = openai.Completion.create(
-          model="text-davinci-003",
+          model="text-curie-001",
           prompt=userInput,
-          temperature=0.9,
+          temperature=1.0,
           max_tokens=150,
           top_p=1,
           frequency_penalty=0.0,
           presence_penalty=0.6,
-          stop=[" Human:", " AI:"]
+          stop=[" You:", " Girlfriend:"]
         )
-        response = data["choices"][0]["text"]; 
-        response = "Girlfriend: " + response
+        response = data["choices"][0]["text"];
+        prompt += "Girlfriend: " + response
+        response = "[magenta]Girlfriend: " + response + "[/magenta]"
         print(response)
     except openai.error.RateLimitError as e:
         print("Rate limit exceeded:", e)
