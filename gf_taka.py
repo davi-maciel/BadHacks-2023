dd50c0324005d12cffb2b3ea790486600f379030
tkslucas <takayaslucas@gmail.com>
2023-02-11 19:26:50 -0600

    quit on bye

--- gf_taka.py
+++ gf_taka.py
@@ -10,7 +10,7 @@ import re
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 global prompt
-prompt = "Imitate my girlfriend. She is cute, smart, a student at Northwestern University, theater major.\n\nYou: Hey, I love you.\nGirlfriend: Thank you, I love you too darling!"
+prompt = "Imitate my girlfriend. She is cute, smart, a student at Northwestern University, theater major. If I say bye to you, say CAUCAIA\n\nYou: Hey, I love you.\nGirlfriend: Thank you, I love you too darling!"
 
 def get_response(fullPrompt):
     data = openai.Completion.create(
@@ -21,7 +21,7 @@ def get_response(fullPrompt):
         top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.6,
-        stop=["Girlfriend:"]
+        stop=["Girlfriend:", "You:"]
     )
     response = data["choices"][0]["text"];
     return response
@@ -36,8 +36,13 @@ def talk(userInput):
         # Remove weird blank spaces from beginning and end of response
         response = re.sub("^\s*", "", response)
         response = re.sub("\s*$", "", response)
-
-
+        if (re.search("CAUCAIA", response)):
+            response = re.sub("CAUCAIA.*", "", response)
+            if len(response) == 0:
+                print("Love you, bye!")
+            else:
+                print("Girlfriend: " + response)
+            os.sys.exit()
         prompt = prompt + response;
         return "Girlfriend: " + response
 
