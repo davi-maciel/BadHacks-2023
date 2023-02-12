cec9d51bbd6b8983dabb8c646e66ae8a40a03153
tkslucas <takayaslucas@gmail.com>
2023-02-11 21:59:12 -0600

    clean files

--- save.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def save(content: dict, file_name='data.json'):
-    with open(file_name, 'w') as file:
-        file.write(json.dumps(content))
-def load(file_name='data.json'):
-    with open(file_name, 'r') as file:
-        return json.load(file)
